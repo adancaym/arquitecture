11c031f53adc750a983c98ed6ba6ea5fa0177c77
Adan Yañez <adanyanezgonzalez@gmail.com>
2021-04-11 21:20:56 -0500

    multitread

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,10 @@ sudo grep -qxF '127.0.0.1       development-caym-server.com' /etc/hosts || sudo 
 sudo grep -qxF '127.0.0.1       test-caym-server.com' /etc/hosts || sudo echo '127.0.0.1       test-caym-server.com' >> /etc/hosts
 sudo grep -qxF '127.0.0.1       mongo-caym-server.com' /etc/hosts || sudo echo '127.0.0.1       mongo-caym-server.com' >> /etc/hosts
 sudo grep -qxF '127.0.0.1       portainer-caym-server.com' /etc/hosts || sudo echo '127.0.0.1       portainer-caym-server.com' >> /etc/hosts
+sudo grep -qxF '127.0.0.1       mail-caym-server.com' /etc/hosts || sudo echo '127.0.0.1       mail-caym-server.com' >> /etc/hosts
+sudo grep -qxF '127.0.0.1       owncloud-caym-server.com' /etc/hosts || sudo echo '127.0.0.1       owncloud-caym-server.com' >> /etc/hosts
+sudo grep -qxF '127.0.0.1       db-owncloud-caym-server.com' /etc/hosts || sudo echo '127.0.0.1       db-owncloud-caym-server.com' >> /etc/hosts
+sudo grep -qxF '127.0.0.1       redis-owncloud-caym-server.com' /etc/hosts || sudo echo '127.0.0.1       redis-owncloud-caym-server.com' >> /etc/hosts
 docker volume create mongo_data
 docker volume create portainer_data
 
