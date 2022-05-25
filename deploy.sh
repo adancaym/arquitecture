7c32cc607eb476c3d3b2f8564d6dc5ba8e37d8a4
adancaym <adancaym@hotmail.com>
2022-05-25 14:38:40 -0500

    net and volumes

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,6 @@ sudo grep -qxF '127.0.0.1       backend.shvl.ia' /etc/hosts || sudo echo '127.0.
 sudo grep -qxF '127.0.0.1       mail.shvl.ia' /etc/hosts || sudo echo '127.0.0.1       mail.shvl.ia' >> /etc/hosts
 sudo grep -qxF '127.0.0.1       mongo.shvl.ia' /etc/hosts || sudo echo '127.0.0.1       mongo.shvl.ia' >> /etc/hosts
 sudo grep -qxF '127.0.0.1       nginx-balancer.shvl.ia' /etc/hosts || sudo echo '127.0.0.1       nginx-balancer.shvl.ia' >> /etc/hosts
-#docker volume create mongo_data
-#docker network create  shvl-net
+docker volume create mongo_data
+docker network create  shvl-net
 #docker-compose up -d
