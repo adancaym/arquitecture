20ed6f8ad1c9af9366e51ccadf4127d58cc88512
adancaym <adancaym@hotmail.com>
2022-05-27 10:29:16 -0500

    fixees

--- backend/mongo.js
+++
@@ -1,7 +0,0 @@
-var MongoClient = require('mongodb').MongoClient;
-var assert = require('assert');
-var url = 'mongodb://shvldb:shvldb2022@shvl.ai:27017/backend-prod';
-
-MongoClient.connect(url,{ useUnifiedTopology: true }, function(err, db) {
-  console.log(err)
-});
