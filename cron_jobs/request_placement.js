1c8867411f51f4a9958610b9fe87257c722531a1
adancaym <adancaym@hotmail.com>
2022-06-02 13:09:57 -0500

    fix host cron jobs

--- cron_jobs/request_placement.js
+++ cron_jobs/request_placement.js
@@ -2,7 +2,9 @@ const axios = require('axios');
 const {SELF_HOST, SELF_PORT, BACKEND_HOST, BACKEND_PORT} = process.env;
 
 
-console.table({SELF_HOST, SELF_PORT, BACKEND_HOST, BACKEND_PORT})
+axios.get(`${BACKEND_HOST}:${BACKEND_PORT}`)
+    .then((({data}) => console.log(data)))
+    .then(() =>
+        axios.get(`${SELF_HOST}:${SELF_PORT}`).then((({data}) => console.log(data)))
+    )
 
-axios.get(`${SELF_HOST}:${SELF_PORT}`).then((({data})=>console.log(data)))
-axios.get(`${BACKEND_HOST}:${BACKEND_PORT}`).then((({data})=>console.log(data)))
