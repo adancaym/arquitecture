e3de63f55b82678d1d5e742158658d0fdc0c7142
adancaym <adancaym@hotmail.com>
2022-06-23 11:27:44 -0500

    cicle fetch collections

--- cron_jobs/request_collection.js
+++ cron_jobs/request_collection.js
@@ -2,27 +2,29 @@ const axios = require("axios");
 const { SELF_HOST, SELF_PORT, BACKEND_HOST, BACKEND_PORT, MASTER_KEY } =
   process.env;
 
-const fetchCollections = async () =>  axios
-.get(`${BACKEND_HOST}:${BACKEND_PORT}/srcCollections/full`, {
-  params: {
-    access_token: MASTER_KEY,
-  },
-})
-.then(async ({ data }) => {
-  console.log("Finished fetching collections");
-  await fetchCollectionsAssets()
-});
+const fetchCollections = async () =>
+  axios
+    .get(`${BACKEND_HOST}:${BACKEND_PORT}/srcCollections/full`, {
+      params: {
+        access_token: MASTER_KEY,
+      },
+    })
+    .then(async ({ data }) => {
+      console.log("Finished fetching collections");
+      await fetchCollectionsAssets();
+    });
 
-const fetchCollectionsAssets = async () => axios
-.get(`${BACKEND_HOST}:${BACKEND_PORT}/assets/collection/assets/`, {
-  params: {
-    access_token: MASTER_KEY,
-  },
-})
-.then(async ({ data }) => {
-  console.log("Finished fetching assets");
-  await fetchCollectionsStats();
-});
+const fetchCollectionsAssets = async () =>
+  axios
+    .get(`${BACKEND_HOST}:${BACKEND_PORT}/assets/collection/assets/`, {
+      params: {
+        access_token: MASTER_KEY,
+      },
+    })
+    .then(async ({ data }) => {
+      console.log("Finished fetching assets");
+      await fetchCollectionsStats();
+    });
 
 const fetchCollectionsStats = async () =>
   axios
@@ -33,11 +35,11 @@ const fetchCollectionsStats = async () =>
     })
     .then(async ({ data }) => {
       console.log("Finished fetching stats");
-      await fetchCollections()
+      await fetchCollections();
     });
 
 try {
-  (()=> async ()=> await fetchCollections())() 
+  (async () => await fetchCollections())();
 } catch (error) {
   console.log(error);
 }
