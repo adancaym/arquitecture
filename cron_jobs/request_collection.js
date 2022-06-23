1590b1251bae8997afe02a7179f24bf7e339fa49
adancaym <adancaym@hotmail.com>
2022-06-23 11:22:48 -0500

    cicle fetch collections

--- cron_jobs/request_collection.js
+++ cron_jobs/request_collection.js
@@ -2,40 +2,42 @@ const axios = require("axios");
 const { SELF_HOST, SELF_PORT, BACKEND_HOST, BACKEND_PORT, MASTER_KEY } =
   process.env;
 
-try {
+const fetchCollections = async () =>  axios
+.get(`${BACKEND_HOST}:${BACKEND_PORT}/srcCollections/full`, {
+  params: {
+    access_token: MASTER_KEY,
+  },
+})
+.then(async ({ data }) => {
+  console.log("Finished fetching collections");
+  await fetchCollectionsAssets()
+});
+
+const fetchCollectionsAssets = async () => axios
+.get(`${BACKEND_HOST}:${BACKEND_PORT}/assets/collection/assets/`, {
+  params: {
+    access_token: MASTER_KEY,
+  },
+})
+.then(async ({ data }) => {
+  console.log("Finished fetching assets");
+  await fetchCollectionsStats();
+});
+
+const fetchCollectionsStats = async () =>
   axios
-    .get(`${BACKEND_HOST}:${BACKEND_PORT}/srcCollections/full`, {
+    .get(`${BACKEND_HOST}:${BACKEND_PORT}/assets/collection/assets/stats`, {
       params: {
         access_token: MASTER_KEY,
-        limit: 2,
       },
     })
     .then(async ({ data }) => {
-      console.log('Finished fetching collections');
-      axios
-        .get(`${BACKEND_HOST}:${BACKEND_PORT}/assets/collection/assets/`, {
-          params: {
-            access_token: MASTER_KEY,
-            limit: 2,
-          },
-        })
-        .then(async ({ data }) => {
-          console.log('Finished fetching assets');
-          axios
-            .get(
-              `${BACKEND_HOST}:${BACKEND_PORT}/assets/collection/assets/stats`,
-              {
-                params: {
-                  access_token: MASTER_KEY,
-                  limit: 2,
-                },
-              }
-            )
-            .then(async ({ data }) => {
-              console.log('Finished fetching stats');
-            });
-        });
+      console.log("Finished fetching stats");
+      await fetchCollections()
     });
+
+try {
+  (()=> async ()=> await fetchCollections())() 
 } catch (error) {
   console.log(error);
 }
