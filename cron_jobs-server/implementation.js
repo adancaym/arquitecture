bf86f55d2f23542419547e67ed1073c5f2a9b422
adancaym <adancaym@hotmail.com>
2022-06-28 07:18:22 -0500

    fix open sea

--- cron_jobs-server/implementation.js
+++ cron_jobs-server/implementation.js
@@ -1,5 +1,5 @@
 const HDWalletProvider = require("@truffle/hdwallet-provider");
-const {OpenSeaPort, Network} = require('opensea-js')
+const {OpenSeaPort} = require('opensea-js')
 
 
 const place_a_bid = async (body) => {
@@ -27,7 +27,9 @@ const web3provider = (infura_key, priv_key) => {
     try {
         const privateKeys = [priv_key.toString()];
         return new HDWalletProvider({
-            privateKeys, providerOrUrl: infura_key.toString(), chainId: process.env.WEB3_PROVIDER,
+            privateKeys,
+            providerOrUrl: infura_key.toString(),
+            chainId: process.env.WEB3_PROVIDER,
         });
     } catch (error) {
         throw new Error('Error creating web3 provider ' + error.message + infura_key + priv_key);
@@ -37,7 +39,8 @@ const web3provider = (infura_key, priv_key) => {
 const createSeaport = (provider, opensea_key) => {
     try {
         return new OpenSeaPort(provider, {
-            networkName: process.env.NETWORK_OPENSEA_PORT, apiKey: opensea_key,
+            networkName: process.env.NETWORK_OPENSEA_PORT,
+            apiKey: opensea_key,
         });
     } catch (error) {
         throw new Error('Error creating seaport ' + error.message);
