fd0d57ce7c1593aa12f5e867689e91dfdc4d2945
adancaym <adancaym@hotmail.com>
2022-06-30 06:19:29 -0500

    logger

--- cron_jobs-server/implementation.js
+++ cron_jobs-server/implementation.js
@@ -48,8 +48,9 @@ const createSeaport = (provider, opensea_key) => {
 }
 
 const place_bid = async (seaport, token_id, token_address, acc_address, bidOffer, exp_time_unix) => {
+    console.table({token_id, token_address, acc_address, bidOffer, exp_time_unix})
     try {
-        return seaport.createBuyOrder({
+        const bid = {
             asset: {
                 tokenId: token_id,
                 tokenAddress: token_address,
@@ -57,42 +58,46 @@ const place_bid = async (seaport, token_id, token_address, acc_address, bidOffer
             accountAddress: acc_address,
             startAmount: parseFloat(bidOffer).toFixed(18),
             expirationTime: exp_time_unix,
-        }).catch((err) => {
+        }
+        console.log(bid)
+        return seaport.createBuyOrder(bid).catch((err) => {
             throw new Error('Error placing bid ' + err.message);
         })
-    } catch (error) {
-        throw new Error('Error placing bid ' + error.message);
+    } catch
+        (error)
+        {
+            throw new Error('Error placing bid ' + error.message);
+        }
+
     }
 
-}
+    async function place_bid_history(seaport, token_id, contract_address, acc_address, desired_bid, max_bid, outbid_value, exp_time, collection_bid) { // exp_time in minutes
+        const expire_time_temp = Math.round(Date.now() / 1000 + 60 * 60 * parseInt(exp_time))
 
-async function place_bid_history(seaport, token_id, contract_address, acc_address, desired_bid, max_bid, outbid_value, exp_time, collection_bid) { // exp_time in minutes
-    const expire_time_temp = Math.round(Date.now() / 1000 + 60 * 60 * parseInt(exp_time))
+        try {
+            if (!collection_bid) {
+                console.log("1")
+                console.table({token_id, contract_address, acc_address, desired_bid, expire_time_temp})
+                return place_bid(seaport, token_id, contract_address, acc_address, desired_bid, expire_time_temp);
+            }
+            const last_bid = collection_bid / 1e18;
+            const bidOffer = parseFloat(last_bid) + parseFloat(outbid_value);
+            if (bidOffer <= desired_bid) {
+                console.log("2")
+                console.table({token_id, contract_address, acc_address, desired_bid, expire_time_temp})
+                return place_bid(seaport, token_id, contract_address, acc_address, desired_bid, expire_time_temp);
+            }
+            if (bidOffer <= max_bid) {
+                console.log("3")
+                console.table({token_id, contract_address, acc_address, bidOffer, expire_time_temp})
+                return place_bid(seaport, token_id, contract_address, acc_address, bidOffer, expire_time_temp);
+            }
+        } catch (error) {
+            throw new Error('Error placing bid history' + error.message + ' ' + expire_time_temp.toString());
+        }
 
-    try {
-        if (!collection_bid) {
-            console.log("1")
-            console.table({token_id, contract_address, acc_address, desired_bid, expire_time_temp})
-            return place_bid(seaport, token_id, contract_address, acc_address, desired_bid, expire_time_temp);
-        }
-        const last_bid = collection_bid / 1e18;
-        const bidOffer = parseFloat(last_bid) + parseFloat(outbid_value);
-        if (bidOffer <= desired_bid) {
-            console.log("2")
-            console.table({token_id, contract_address, acc_address, desired_bid, expire_time_temp})
-            return place_bid(seaport, token_id, contract_address, acc_address, desired_bid, expire_time_temp);
-        }
-        if (bidOffer <= max_bid) {
-            console.log("3")
-            console.table({token_id, contract_address, acc_address, bidOffer, expire_time_temp})
-            return place_bid(seaport, token_id, contract_address, acc_address, bidOffer, expire_time_temp);
-        }
-    } catch (error) {
-        throw new Error('Error placing bid history' + error.message + ' ' + expire_time_temp.toString());
     }
 
-}
-
-module.exports = {
-    place_a_bid,
-}
+    module.exports = {
+        place_a_bid,
+    }
